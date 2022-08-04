c48bfda15576e80f6bf22eba82547c4d2b1f159d
sujoyhalder88 <sujoyhalder88@gmail.com>
2022-08-04 13:21:08 +0530

    4th

---
+++ 4thAug.js
@@ -0,0 +1,19 @@
+//Q:17. Write a JavaScript function to  get the number of occurrences of each letter in specified string.
+
+// function specifiedString(str) {
+//     let occr = {};
+//     for (let i = 0; i < str.length; i++) {
+//         if (!occr[str[i]]) {
+//             occr[str[i]] = 1;
+//         } else {
+//             occr[str[i]] += 1;
+//         }
+//     }
+//     return occr;
+// }
+// console.log(specifiedString("thequickbrownfoxjumpsoverthelazydog"));
+
+//=======================XXXXXXX======================================
+//Q:18. Write a function for searching JavaScript arrays with a binary search
+//Note : A binary search searches by splitting an array into smaller and smaller chunks until it
+// finds the desired value
